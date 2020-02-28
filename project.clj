26f9c89ff967c98dad74cc6b3c1b0f6255585a04
ronen <narkisr@gmail.com>
2020-02-28 22:31:49 +1100

    Table print and more error resilient code

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url ""
   :license {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [
-      [org.clojure/clojure "1.10.1"]
-  ]
-  :repl-options {:init-ns clj-stat.core})
+      [rm-hull/table "0.7.0"]
+      [org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns clj-stat.core}
+  :main clj-stat.core
+  :aot [clj-stat.core])
--- src/clj_stat/core.clj
+++ src/clj_stat/core.clj
@@ -1,38 +1,73 @@
 (ns clj-stat.core
+  (:gen-class)
   (:require
+   [table.core :refer [table]]
    [clojure.string :as str]
    [clojure.java.shell :refer (sh)]))
 
 (defn jps []
   (let [{:keys [exit out err]} (sh "jps")]
-    (map (fn [p] (zipmap [:pid :name] (str/split p #" "))) (str/split out #"\n"))))
+    (if-not (= exit 0)
+      {}
+      (map (fn [p] (zipmap [:pid :name] (str/split p #" "))) (str/split out #"\n")))))
 
 (defn jstat
   "https://docs.oracle.com/javase/8/docs/technotes/tools/unix/jstat.html"
   [pid]
   (let [{:keys [exit out err]} (sh "jstat" "-gc" pid "1" "1")]
-    (zipmap
-     [:S0C :S1C :S0U :S1U :EC :EU :OC :OU :MC :MU :CCSC :CCSU :YGC :YGCT :FGC :FGCT :GCT]
-     (map (fn [n] (BigDecimal. n)) (filter (comp not empty?) (str/split (second (str/split out #"\n")) #" "))))))
+    (when (= exit 0)
+      (zipmap
+       [:S0C :S1C :S0U :S1U :EC :EU :OC :OU :MC :MU :CCSC :CCSU :YGC :YGCT :FGC :FGCT :GCT]
+       (map (fn [n] (BigDecimal. n)) (filter (comp not empty?) (str/split (second (str/split out #"\n")) #" ")))))))
 
 (defn used-heap [m]
-  (/ (apply + (vals (select-keys m [:S0U :S1U :EU :OU]))) 1024.0))
+  (when m
+    (/ (apply + (vals (select-keys m [:S0U :S1U :EU :OU]))) 1024.0)))
 
-(defn ram
+(defn statm
   [pid]
-  (zipmap
-   [:size :resident :shared :text :lib :data :dt]
-   (map (fn [n] (BigDecimal. n)) (str/split (str/trim (slurp (java.io.FileReader. (str "/proc/" pid "/statm")))) #" "))))
+  (try
+    (let [f (java.io.FileReader. (str "/proc/" pid "/statm"))]
+      (zipmap
+       [:size :resident :shared :text :lib :data :dt]
+       (map (fn [n] (BigDecimal. n)) (str/split (str/trim (slurp f)) #" "))))
+    (catch java.io.FileNotFoundException e nil)))
 
-(defn used-ram [{:keys [resident data text shared]}]
-  (/ (+ resident data text shared) 1024.0))
+(defn used-ram [m]
+  (when m
+    (let [{:keys [resident data text shared]} m]
+      (/ (+ resident data text shared) 1024.0))))
 
 (defn cpu-use [pid]
-  (let [{:keys [exit out err]} (sh "ps" "-p" pid "-o" "%cpu")]
-    (BigDecimal. (second (str/split (str/trim out) #" ")))))
+  (let [{:keys [exit out]} (sh "ps" "-p" pid "-o" "%cpu")]
+    (when (= exit 0)
+      (when-let [c (second (str/split (str/trim out) #" "))]
+        (BigDecimal. c)))))
+
+(defn usage [{:keys [pid] :as m}]
+  (merge m {:heap (used-heap (jstat pid)) :ram (used-ram (statm pid)) :cpu (cpu-use pid)}))
+
+(defn mb [i]
+  (str (int i) " MB"))
+
+(defn display [m]
+  (-> m
+      (update :heap mb)
+      (update :ram mb)
+      (update :cpu float)))
+
+(defn java-top []
+  (table
+   (map display
+        (filter (fn [p] (not (some nil? (vals p)))) (map usage (jps)))) :style :borderless))
+
+(defn -main [& args]
+  (java-top)
+  (System/exit 0))
 
 (comment
+  (java-top)
   (jps)
-  (cpu-use "23989")
+  (cpu-use "12744")
   (used-heap (jstat "24032"))
-  (used-ram (ram "24032")))
+  (used-ram (statm "24032")))
