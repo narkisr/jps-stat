db8216c3b698352ddb07c3925b72d0d21fd3f72b
ronen <narkisr@gmail.com>
2020-03-03 01:41:17 +1100

    Downgrading clojure to 1.9.0 due to https://clojure.atlassian.net/browse/CLJ-2482?oldIssueView=true

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
 
   :dependencies [
-      [org.clojure/clojure "1.10.1"]
+      [org.clojure/clojure "1.9.0"]
       [rm-hull/table "0.7.0"]
       [cli-matic "0.3.11"]
   ]
